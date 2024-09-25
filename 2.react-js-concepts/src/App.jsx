cb1728bf725637cc02a57be79c42a7b4d5320b63
Abhinav Singh <abhisingh622000@gmail.com>
2024-09-26 02:31:37 +0530

    About types of components

---
+++ 2.react-js-concepts/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import ClassBasedComponent from './components/class-based-component'
+import FunctionalBased from './components/Functional-based-component'
+
+function App() {
+
+
+  return (
+    <div>
+      <ClassBasedComponent />
+      <FunctionalBased />
+    </div>
+  )
+}
+
+export default App
+
