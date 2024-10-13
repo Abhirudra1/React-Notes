6458b67500a1b1485d639794015bfae767afd775
Abhinav Singh <abhisingh622000@gmail.com>
2024-10-13 23:27:02 +0530

    mini changes in index.jsx

--- 2.react-js-concepts/src/components/products/index.jsx
+++ 2.react-js-concepts/src/components/products/index.jsx
@@ -7,10 +7,34 @@ import './style.css'
 
 
 function ProductList({ name, city, dummyProductData}) {
+    const flag = false
+
+    // function getText(getFlag){
+    //     return getFlag ? <h4>I am true</h4> : <h4>I am false</h4>
+    // }
+    //  OR
+
+    // const getText = flag? <h4>I am true</h4> : <h4>I am false</h4>
+    
+    // OR using if-else
+
+    
+
+    let getText = null
+    if(flag){
+        getText = <h4>I am true if</h4>
+    } else{
+        getText = <h4>else I am false </h4>
+    }
+
     return ( 
         <div>
             <h3 className="title">ECommerce Project</h3>
             <p>My name is {name}. I am from {city}</p>
+
+            {/* {getText(flag)} */}
+            {getText}
+
             <ul>
                 {
                     dummyProductData.map((item, index)=>(
